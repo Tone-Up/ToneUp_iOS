c6f1a00b6fba9412f58ad21d3ee827c05de754b9
JunYeong-J <eeot2827@gmail.com>
2025-05-12 16:36:58 +0900

    init: test 초기 세팅

---
+++ ToneUp/ToneUpTests/ToneUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToneUpTests.swift
+//  ToneUpTests
+//
+//  Created by 전준영 on 5/12/25.
+//
+
+import Testing
+@testable import ToneUp
+
+struct ToneUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
