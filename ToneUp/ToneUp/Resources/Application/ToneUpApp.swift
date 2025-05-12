d0c39c964f733392d19550e906dff45904ad81c1
JunYeong-J <eeot2827@gmail.com>
2025-05-12 16:36:37 +0900

    init: 초기세팅

---
+++ ToneUp/ToneUp/Resources/Application/ToneUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneUpApp.swift
+//  ToneUp
+//
+//  Created by 전준영 on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToneUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
