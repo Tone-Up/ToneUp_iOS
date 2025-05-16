b90d110a6b9ca216f3d9882aab28b17b885aa188
JunYeong-J <eeot2827@gmail.com>
2025-05-16 21:31:55 +0900

    feat: 공통버튼 Component 추가

--- ToneUp/ToneUp/Sources/Components/Button/CommonButton.swift
+++ ToneUp/ToneUp/Sources/Components/Button/CommonButton.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct CommonButton: View {
+    
+    let icon: Image? //글자만 있는 버튼도 사용할 수 있도록
+    let backgroundColor: Color //배경색
+    let disabledBackgroundColor: Color? = .gray// disabled일때 배경색상
+    let text: ButtonTitle //글자
+    let textColor: Color //글자색상
+    let symbolColor: Color? //로고 색상
+    let cornerRadius: CGFloat //모서리
+    var isEnabled: Bool = true
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            if isEnabled {
+                action()
+            }
+        }) {
+            HStack {
+                if let icon = icon {
+                    icon
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundStyle(symbolColor ?? .primary)
+                }
+                
+                Text(text.rawValue)
+                    .font(.headline)
+                    .foregroundStyle(textColor.opacity(0.85))
+                
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(isEnabled ? backgroundColor : (disabledBackgroundColor ?? backgroundColor))
+            .cornerRadius(cornerRadius)
+        }
+        .disabled(!isEnabled)
     }
+    
 }
 
 #Preview {
-    CommonButton()
+    CommonButton(icon: nil,
+                 backgroundColor: .red,
+                 text: .goLogin,
+                 textColor: .green,
+                 symbolColor: .blue,
+                 cornerRadius: 20,
+                 action: {
+        
+    })
 }
