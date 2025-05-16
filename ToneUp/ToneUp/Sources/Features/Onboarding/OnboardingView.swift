929049afc447bb2afcb34df7444c649f662b439d
JunYeong-J <eeot2827@gmail.com>
2025-05-16 21:33:38 +0900

    feat: 온보딩뷰 생성

--- ToneUp/ToneUp/Sources/Features/Onboarding/OnboardingView.swift
+++ ToneUp/ToneUp/Sources/Features/Onboarding/OnboardingView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack() {
+            Spacer()
+            
+            CommonText(text: AppText
+                .Logo
+                .title
+                .rawValue,
+                       font: .notoMedium60,
+                       color: .black)
+            
+            CommonText(text: AppText
+                .Onboarding
+                .subTitle
+                .rawValue,
+                       font: .notoRegular18,
+                       color: .black)
+            
+            Image.onboardingImage
+            
+            Spacer()
+            
+            CommonButton(icon: nil,
+                         backgroundColor: .gray,
+                         text: .goLogin,
+                         textColor: .black,
+                         symbolColor: nil,
+                         cornerRadius: 24) {
+                
+            }
+                         .padding(20)
+        }
     }
+    
 }
 
 #Preview {
