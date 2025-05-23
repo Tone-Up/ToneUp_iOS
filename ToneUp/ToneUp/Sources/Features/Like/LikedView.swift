b1937b3a5aa712d925be6912fef9e3d255f4788c
JunYeong-J <eeot2827@gmail.com>
2025-05-23 15:48:08 +0900

    fix: 스크롤 버그 수정

--- ToneUp/ToneUp/Sources/Features/Like/LikedView.swift
+++ ToneUp/ToneUp/Sources/Features/Like/LikedView.swift
@@ -39,6 +39,7 @@ struct LikedView: View {
                             }
                         }
                     }
+                    .id(selectedTab)
                     .scrollContentBackground(.hidden)
                 }
             }
