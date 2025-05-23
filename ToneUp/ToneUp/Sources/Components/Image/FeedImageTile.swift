e33c0742eca13068f0a84f50999119de90170b50
JunYeong-J <eeot2827@gmail.com>
2025-05-23 15:47:45 +0900

    refactor: 피드 이미지 '아닐시' 옵션추가

--- ToneUp/ToneUp/Sources/Components/Image/FeedImageTile.swift
+++ ToneUp/ToneUp/Sources/Components/Image/FeedImageTile.swift
@@ -10,24 +10,38 @@ import SwiftUI
 struct FeedImageTile: View {
     
     let image: Image
+    var isFeed: Bool? = true
     var showHeart: Bool = true
     var width: CGFloat? = nil
     var height: CGFloat
-
+    
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
-            image
-                .resizable()
-                .scaledToFill()
-                .frame(width: width, height: height)
-                .clipped()
-
-            if showHeart {
-                Image(systemName: "heart.fill")
-                    .foregroundColor(.red)
-                    .padding(6)
+            if isFeed ?? true {
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: width, height: height)
+                    .clipped()
+                
+                
+                if showHeart {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                        .padding(6)
+                }
+            } else {
+                VStack {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: width, height: height)
+                        .clipped()
+                    
+                    CommonText(text: "제목")
+                }
             }
         }
+        
     }
-    
 }
