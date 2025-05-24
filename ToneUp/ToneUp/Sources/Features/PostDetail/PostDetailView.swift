0cfd21719d75e890a33a3e3a2adec81166fe593d
JunYeong-J <eeot2827@gmail.com>
2025-05-24 20:44:27 +0900

    feat: 폰트 추가

---
+++ ToneUp/ToneUp/Sources/Features/PostDetail/PostDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PostDetailView.swift
+//  ToneUp
+//
+//  Created by 전준영 on 5/24/25.
+//
+
+import SwiftUI
+
+struct PostDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostDetailView()
+}
