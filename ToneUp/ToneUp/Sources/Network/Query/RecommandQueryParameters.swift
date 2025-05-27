59d0d5658db2f36a3de0437cfc2fb52eab7e2598
JunYeong-J <eeot2827@gmail.com>
2025-05-27 23:17:54 +0900

    feat: query파라미터 정의

--- ToneUp/ToneUp/Sources/Network/Query/RecommandQueryParameters.swift
+++ ToneUp/ToneUp/Sources/Network/Query/RecommandQueryParameters.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct RecommandQueryParameters: QueryStringProtocol {
+    
+    let cursor: Int
+    let limit: Int
+    let sort: String
+    
+    func asQueryItems() -> [String : Any] {
+        var items: [String : Any] = [:]
+        
+        items["cursor"] = cursor
+        items["limit"] = limit
+        items["sort"] = sort
+        
+        return items
+    }
+    
+}
