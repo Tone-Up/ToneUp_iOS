b23e188844b0e57978124cbc20cbf4e0c7cb87b6
JunYeong-J <eeot2827@gmail.com>
2025-05-27 23:17:38 +0900

    feat: NetworkProvider async/await로 정의

--- ToneUp/ToneUp/Sources/Network/Provider/NetworkProvider.swift
+++ ToneUp/ToneUp/Sources/Network/Provider/NetworkProvider.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+import Moya
+
+final class NetworkProvider<T: TargetType> {
+    
+    private let provider: MoyaProvider<T>
+    private let tokenRetrier = AccessTokenRetrier()
+    
+    init() {
+        let session = Session(interceptor: tokenRetrier)
+        let plugins: [PluginType] = [NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))]
+        self.provider = MoyaProvider<T>(
+            session: session,
+            plugins: plugins
+        )
+    }
+    
+    func request<R: Decodable>(
+        _ target: T,
+        decodingType: R.Type,
+        retryCount: Int = 1
+    ) async throws -> R {
+        var attempt = 0
+
+        while true {
+            do {
+                let response = try await provider.asyncRequest(target)
+                let serverResponse = try response.map(ServerResponse<R>.self)
+                
+                return try await handleResponse(serverResponse)
+            } catch {
+                attempt += 1
+                if attempt > retryCount {
+                    throw APIErrorMapper.map(error)
+                }
+            }
+        }
+    }
+    
+}
+
+extension MoyaProvider {
+    func asyncRequest(_ target: Target) async throws -> Response {
+        try await withCheckedThrowingContinuation { continuation in
+            self.request(target) { result in
+                switch result {
+                case .success(let response):
+                    continuation.resume(returning: response)
+                    
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
