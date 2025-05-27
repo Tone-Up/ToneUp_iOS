8b80aaf0880f192daf543630de229ae42d623d5a
JunYeong-J <eeot2827@gmail.com>
2025-05-27 23:16:11 +0900

    feat: 헤더 정의

--- ToneUp/ToneUp/Sources/Network/Header/HeaderKeyAndValue.swift
+++ ToneUp/ToneUp/Sources/Network/Header/HeaderKeyAndValue.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum Header {
+    
+    case contentTypeJson
+    case contentTypeMulti
+    case authorization
+    
+    var key: String {
+        switch self {
+        case .contentTypeJson,
+                .contentTypeMulti:
+            return "Content-Type"
+            
+        case .authorization:
+            return "Authorization"
+        }
+    }
+    
+    var value: String {
+        switch self {
+        case .contentTypeJson:
+            return "application/json"
+            
+        case .contentTypeMulti:
+            return "multipart/form-data"
+            
+        case .authorization:
+            guard let token = KeychainManager.load(forKey: .accessToken) else { return "실패" }
+            return "Bearer \(token)"
+        }
+    }
+    
+}
